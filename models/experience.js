f6125cb65d51523607f1a00b85c5306faea70546
Gabriel John Manzanares <gjamanzanares@gmail.com>
2025-05-17 12:18:19 +0800

    added api for experiences

---
+++ models/experience.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const positionSchema = new mongoose.Schema({
+    position: {
+        required: true,
+        type: String
+    },
+    pos_start: {
+        required: true,
+        type: Date
+    },
+    pos_end: { type: Date },
+    pos_len: { type: String },
+    position_desc: [{ type: String }]
+});
+
+const dataSchema = new mongoose.Schema({
+    job_number: {
+        required: true,
+        type: Number,
+    },
+    job: {
+        required: true,
+        type: String
+    },
+    company: {
+        required: true,
+        type: String
+    },
+    start: {
+        required: true,
+        type: Date
+    },
+    end: {
+        type: Date
+    },
+    len: {
+        required: true,
+        type: String
+    },
+    job_type: {
+        required: true,
+        type: String
+    },
+    positions: [positionSchema]
+});
+
+module.exports = mongoose.model('Experience', dataSchema);
--- routes/routes.js
+++ routes/routes.js
@@ -3,17 +3,98 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const moment = require('moment');
 
 const Project = require('../models/project');
+const Experience = require('../models/experience');
 
 module.exports = router;
 
-const encode = (url) => {
-    const base64Img = require('base64-img');
-    let data = base64Img.base64Sync(url);
-    return data;
-}
+// GET all
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Project.find();
+        const experiences = await Experience.find().sort({ job_number: -1 });
+        // res.json(data);
+        res.render('main', { moment: moment, items: { projects: projects, experiences: experiences }});
+    }
+    catch(error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
+// == EXPERIENCES == //
+// POST to Experiences
+router.post('/experiences/post', async (req, res) => {
+    const data = new Experience({
+        job_number: req.body.job_number,
+        job: req.body.job,
+        company: req.body.company,
+        start: req.body.start,
+        end: req.body.end,
+        len: req.body.len,
+        job_type: req.body.job_type,
+        positions: req.body.positions
+    });
+
+    try {
+        await data.save();
+        res.redirect('/');
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
+// GET Experience by ID
+router.get('/experiences/:id', async (req, res) => {
+    try {
+        const experience = await Experience.findById(req.params.id);
+        // res.json(data);
+        // will change to render on different page to show single experience
+        res.render('main', { items: { projects: [], experiences: experience } });
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// UPDATE Experience by ID
+router.patch('/experiences/:id/update', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Experience.findByIdAndUpdate(
+            id, 
+            updatedData,
+            options
+        );
+
+        // res.send(result);
+        // will change
+        // res.render('main', { items: { projects: [], experiences: result } });
+        res.redirect('/');
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// DELETE Experience by ID
+router.delete('/experiences/:id/del', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Experience.findByIdAndDelete(id);
+        // res.send(`Document with ${data.name} has been deleted...`);
+        // will change
+        res.render('main', { items: { projects: [], experiences: data } });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+})
+// ==== //
+
+// == PROJECTS == //
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads')
@@ -25,18 +106,6 @@ let storage = multer.diskStorage({
 });
 
 let upload = multer({ storage: storage });
-
-// GET all
-router.get('/', async (req, res) => {
-    try {
-        const data = await Project.find();
-        // res.json(data);
-        res.render('main', { items: data });
-    }
-    catch(error) {
-        res.status(500).json({message: error.message});
-    }
-});
 
 // POST to Projects
 router.post('/projects/post', upload.single('img'), async (req, res) => {
@@ -53,37 +122,24 @@ router.post('/projects/post', upload.single('img'), async (req, res) => {
         await data.save();
         // res.status(200).json(dataToSave);
         res.redirect('/');
-    }
-    catch (error) {
+    } catch (error) {
         res.status(400).json({message: error.message});
     }
 });
 
-router.get('/admin', async (req, res) => {
+// GET Project by ID
+router.get('/projects/:id', async (req, res) => {
     try {
-        const data = await Project.find();
+        const projects = await Project.findById(req.params.id);
         // res.json(data);
-        res.render('admin', { items: data });
-    }
-    catch(error) {
+        res.render('main', { items: { projects: projects, experiences: [] } });
+    } catch(error) {
         res.status(500).json({message: error.message});
     }
 });
 
-// GET by ID
-router.get('/projects/get/:id', async (req, res) => {
-    try {
-        const data = await Project.findById(req.params.id);
-        // res.json(data);
-        res.render('main', { items: data });
-    }
-    catch(error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-// UPDATE by ID
-router.patch('/projects/update/:id', upload.single('img'), async (req, res) => {
+// UPDATE Project by ID
+router.patch('/projects/:id/update', upload.single('img'), async (req, res) => {
     try {
         if (req.fileIdentifier != undefined) {
             req.body.img = {
@@ -103,21 +159,21 @@ router.patch('/projects/update/:id', upload.single('img'), async (req, res) => {
             options
         );
 
-        res.send(result);
-    }
-    catch (error) {
+        // res.send(result);
+        res.render('main', { items: { projects: result, experiences: [] } });
+    } catch (error) {
         res.status(400).json({ message: error.message });
     }
 });
 
-// DELETE by ID
+// DELETE Project by ID
 router.delete('/projects/delete/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const data = await Project.findByIdAndDelete(id);
-        res.send(`Document with ${data.name} has been deleted..`);
-    }
-    catch (error) {
+        // res.send(`Document with ${data.name} has been deleted..`);
+        res.render('main', { items: { projects: data, experiences: [] } });
+    } catch (error) {
         res.status(400).json({ message: error.message });
     }
-})
+});
